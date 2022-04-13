1bc6af4d2fb82ec8257ddb68db30e3aae4601833
Onika Kim Asiao Dumbrique <kim.asiao@partners.w2m.com>
2022-04-13 11:43:11 +0200

    Done the routing of the app

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentes
+import {ListarProductosComponent} from "./components/listar-productos/listar-productos.component";
+import {CrearProductoComponent} from "./components/crear-producto/crear-producto.component";
+
+const routes: Routes = [ //agregamos un obj
+  //path:: /url , component:: what component gonna use (which template)
+  { path: '', component:ListarProductosComponent },
+  { path: 'crear-producto', component:CrearProductoComponent },
+  { path: 'editar-producto/:id', component:CrearProductoComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }, //IF WRONG URL, then gonna redirect to ' / ' . Then, pathMath:: comparamos toda la URL
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
